3a9cf7d262765705c34ee252d1bc1e5e2543573a
SamyraFigueredo <samyrafigueiredo24@gmail.com>
2025-02-04 10:49:55 -0300

    Configurando WebSocket para comunicacao£o em tempo real no jogo da vea

--- src/main/java/fnrc/JogoDaVelha/WebSocketConfig.java
+++ src/main/java/fnrc/JogoDaVelha/WebSocketConfig.java
@@ -1,2 +1,23 @@
-package fnrc.JogoDaVelha;public class WebSocketConfig {
+package fnrc.JogoDaVelha;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/mensage-serve");
+        config.setApplicationDestinationPrefixes("/mensage-client");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/Jogo-da-velha").withSockJS();
+    }
 }
